51fee53d36ac4ec23479cc352617af4248d3b646
Marcus Vinicius José <marcus.jose@etec.sp.gov.br>
2025-09-16 12:01:10 -0300

    HelpDesk 0.0.8
    Teste e correções de erros.

--- App/core/Database.php
+++ App/core/Database.php
@@ -6,7 +6,7 @@ class Database{
 
     public static function conn(): PDO{
         if(!self::$pdo){
-            $dsn = "mysql:host=" . DB_HOST . ";db_name=" . DB_NAME . ";charset=utf8mb4";
+            $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8mb4";
             self::$pdo = new PDO($dsn, DB_USER, DB_PASS, [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
             ]);
--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+session_start();
+
 require __DIR__ . '/../config/config.php';
 require __DIR__ . '/../app/core/Router.php';
 
