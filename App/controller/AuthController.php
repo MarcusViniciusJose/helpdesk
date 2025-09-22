584a1bc657eae5bd4ac5285b379742ab5c4a865d
Marcus Vinicius José <marcus.jose@etec.sp.gov.br>
2025-09-22 08:55:25 -0300

    HelpDesk 0.0.9
    Mais testes e correções de erros.

--- App/controller/AuthController.php
+++ App/controller/AuthController.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once __DIR__ . '/../core/Auth.php';
 require_once __DIR__ . '/../models/User.php';
+require_once __DIR__ . '/../core/Auth.php';
 class AuthController{
 
     public function login(){
